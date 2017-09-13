4291bcfdd1b668700793ae6a254ce86151bd1a26
Tatyana <tatyana8787@gmail.com>
2017-09-13 22:20:34 +0200

    js

--- js/common.js
+++ js/common.js
@@ -2,4 +2,60 @@ $('[data-fancybox]').fancybox({
 	buttons : [
 		'close'
 	]
-	});
+});
+
+$('.certificates__list').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+		{
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+		{
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+	]
+});
+
+$('.partners__list').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+		{
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+		{
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+	]
+});
