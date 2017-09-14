b3ebdfb5bc595f11f4a697d9c4cf83c3004b8eb4
Tatyana <tatyana8787@gmail.com>
2017-09-14 18:29:42 +0200

    js

--- js/common.js
+++ js/common.js
@@ -1,61 +1,84 @@
-$('[data-fancybox]').fancybox({
-	buttons : [
-		'close'
-	]
-});
+$(document).ready(function() {
 
-$('.certificates__list').slick({
-	slidesToShow: 4,
-	slidesToScroll: 1,
-	responsive: [
-		{
-      breakpoint: 1080,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 780,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-		{
-      breakpoint: 560,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-	]
-});
+	//E-mail Ajax Send
+	$(".contact-form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find(".success").addClass("active").css('display', "flex").hide().fadeIn();
+			setTimeout(function() {
+				$(th).find(".success").removeClass("active").fadeOut();
+				// Done Functions
+				th.trigger("reset");
+			}, 2000);
+		});
+		return false;
+	});
 
-$('.partners__list').slick({
-	slidesToShow: 4,
-	slidesToScroll: 1,
-	responsive: [
-		{
-      breakpoint: 1080,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 780,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-		{
-      breakpoint: 560,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-	]
+
+	$('[data-fancybox]').fancybox({
+		buttons : [
+			'close'
+		]
+	});
+
+	$('.certificates__list').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1080,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 780,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 560,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+	$('.partners__list').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1080,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 780,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 560,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
 });
