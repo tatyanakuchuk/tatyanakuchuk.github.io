5e908c04f4ba8ec1296910ad9403366d21c001c9
Tatyana <tatyana8787@gmail.com>
2017-10-14 22:43:45 +0200

    1

--- js/common.js
+++ js/common.js
@@ -30,6 +30,44 @@ $(document).ready(function() {
 		}]
 	});
 
+	$('.mob-cloud-advantages').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1
+				}
+			},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 570,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+
+
 	// СТРОКА ПОИСКА ГОРОДА
 // 	$('#search-city-input').hideseek({
 // 		nodata: 'По вашему запросу ничего не найдено',
